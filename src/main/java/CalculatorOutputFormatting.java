a5406855d7119424e6e5aecd024af89923ba6953
thewhitehaven04 <thewhitehaven04@gmail.com>
2022-04-04 02:10:44 +0300

    Выполнено задание Задание №1_Калькулятор

---
+++ src/main/java/CalculatorOutputFormatting.java
@@ -0,0 +1,12 @@
+/**
+ * @author German Bulavkin
+ */
+
+/**
+ * This class contains constants for setting calculator output formatting defaults.
+ */
+public class CalculatorOutputFormatting {
+
+    // Default format for outputting floats
+    static public String DEFAULT_FLOAT_OUTPUT_FORMAT = "%.4f";
+}
